16df24f18714808e39696f2740420594911a8c3e
Bryan Berger <bryan@bryanberger.com>
2021-10-01 15:32:21 -0400

    init

---
+++ README.md
@@ -0,0 +1,38 @@
+# Figma Patcher (PoC)
+
+## Disclaimer
+
+This is an educational proof-of-concept style patcher for Figma. It allows one to expand the width of the Figma properties panel sidebar (in this case to 350px).
+This application does not modify any Figma files, it uses the `Electron / Chromium` debugger to inject some javascript and styling into each Figma file/page.
+
+## How to run
+
+Ensure you run Figma.app in remote debug mode like so:
+
+- Close Figma
+- Open your Terminal
+- Type in `open /Applications/Figma.app --args --remote-debugging-port=9222` assuming that is the location of your Figma.app
+
+Figma will now open in `remote debugging mode`.
+
+You can now run this application binary, which will open in a terminal prompt, keep the window open and minimize it, you should be good to go now.
+
+## Development
+
+Install dependencies and start.
+
+You can add additional payloads by creating a new payload javascript file in `payloads/` it will get auto-loaded and included once per file/page. Make sure these are properly formatted JS files terminated with a semicolon. There is room to improve here with a proper plugin architecture, but this is a proof-of-concept, and good enough for now.
+
+```
+npm install
+npm start
+```
+
+## Build
+
+```
+npm install
+npm run build
+```
+
+This should create 2 binaries, one for macOS and one for Windows 10.
---
+++ index.js
@@ -0,0 +1,76 @@
+const WebSocket = require("ws");
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+const fileListUrl = `http://localhost:9222/json`;
+const fileSocketMap = {};
+const payload = {
+  id: 1337,
+  method: "Runtime.evaluate",
+  params: { expression: "" },
+};
+
+let fileList;
+let payloads = "";
+
+const sendPayload = (id, client) => client.send(JSON.stringify(payload));
+
+const buildPayloads = () => {
+  const dir = "./payloads/";
+  const filenames = fs.readdirSync(dir);
+
+  filenames.forEach((filename) => {
+    const payload = fs.readFileSync(dir + filename).toString();
+    payloads += payload;
+  });
+
+
+  // Wrapper for payloads, so they only run once per file/page
+  payload.params.expression = `
+    (function () {
+        if (typeof window.hasBeenPatched === "undefined") {
+            
+            ${payloads}
+
+            window.hasBeenPatched = true;
+        }
+    })();
+  `;
+};
+
+const pollAndInject = async () => {
+  let response = await fetch(fileListUrl);
+
+  fileList = await response.json();
+
+  fileList.forEach((file) => {
+    // Skip official Figma pages
+    if (
+      file.title === "Figma" ||
+      file.title === "DevTools" ||
+      file.title === "Recently viewed – Figma" ||
+      file.url === "https://www.figma.com/community"
+    )
+      return;
+
+    const client = fileSocketMap[file.id];
+
+    if (client && client.readyState === WebSocket.OPEN) {
+      sendPayload(file.id, client);
+    } else {
+      const newClient = new WebSocket(file.webSocketDebuggerUrl);
+      newClient.on("open", async () => sendPayload(file.id, newClient));
+      fileSocketMap[file.id] = newClient;
+    }
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, 3000)); // fetch list of files every 3s
+  await pollAndInject();
+};
+
+buildPayloads();
+pollAndInject();
+
+console.log(
+  "Patcher Running...please keep this window open, you can minimize it if you want."
+);
